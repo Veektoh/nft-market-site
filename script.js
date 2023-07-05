3e5067ee2fa83b77c73b7a355f150582d8289ac7
Victor ayo <58230420+Veektoh@users.noreply.github.com>
2023-07-05 12:27:40 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
+//dark mode toggle 
+const body = document.body;
+const header= document.querySelector('header');
+let darkMode = localStorage.getItem('darkMode');
+const darktoggler = document.querySelector('.dark-mode-toggle');
+const darkTogglerIcon = document.querySelector('.dark-toggle-icon');
 
-// menu button toggle
+function enableDarkMode(){
+    body.classList.add(('dark-theme'));
+    localStorage.setItem('darkMode', 'enabled');
+    header.style.backgroundImage = "url(images/Backgrounddark.svg)";
+    darkTogglerIcon.classList.add('fa-sun');
+    darkTogglerIcon.classList.remove('fa-moon');
+}
+function disabledDarkMode (){
+    body.classList.remove(('dark-theme'));
+    localStorage.setItem('darkMode', null)
+    header.style.backgroundImage = "url(images/Background.svg)";
+    darkTogglerIcon.classList.add('fa-moon');
+    darkTogglerIcon.classList.remove('fa-sun');
+}
+if (darkMode === 'enabled'){
+    enableDarkMode();
+}
+darktoggler.addEventListener('click', (e)=>{
+    darkMode = localStorage.getItem('darkMode');
+    if (darkMode !== 'enabled') {
+        enableDarkMode();
+    }
+    else{
+        disabledDarkMode ();
+    }
+})
+
+ // menu button toggle
 function toggleMenu () {
     const menu = document.querySelector("#menu-wrapper");
     menu.classList.toggle("show-menu");
@@ -22,7 +55,7 @@ const latestAuctions = [
     },
     {
         id: 2,
-        title: "Tristique diam a, enim, eros tellus. Viverra etiam",
+        title: "Tellus duis neque fringilla pulvinar gravida",
         price: "4.2",
         avatar : ["09", "06", "08"],
         time: "20:41",
@@ -38,7 +71,7 @@ const latestAuctions = [
     },
     {
         id: 4,
-        title: "Tristique diam a, enim, eros tellus. Viverra etiam",
+        title: "Mattis at diam lorem nisl nam sed sociis",
         price: "10.21",
         avatar : ["09", "06", "08"],
         time: "7:01",
@@ -46,7 +79,7 @@ const latestAuctions = [
     },
     {
         id: 5,
-        title: "Tristique diam a, enim, eros tellus. Viverra etiam",
+        title: "Diam purus sagittis nisl erat mauris",
         price: "41.22",
         avatar : ["09", "06", "08"],
         time: "3:21",
@@ -54,7 +87,7 @@ const latestAuctions = [
     },
     {
         id: 6,
-        title: "Tristique diam a, enim, eros tellus. Viverra etiam",
+        title: "Senectus adipiscing nascetur accumsan etiam",
         price: "4.2",
         avatar : ["09", "06", "08"],
         time: "2:41",
@@ -62,11 +95,107 @@ const latestAuctions = [
     },
     {
         id: 7,
-        title: "Tristique diam a, enim, eros tellus. Viverra etiam",
+        title: "Dui accumsan leo vestibulum ornare eu",
         price: "6.2",
         avatar : ["09", "06", "08"],
         time: "9:41",
         likes: "19"
+    },
+    {
+        id: 8,
+        title: "Tristique diam a, enim, eros tellus. Viverra etiam",
+        price: "3.85",
+        avatar : ["09", "06", "08"],
+        time: "1:41",
+        likes: "30"
+    },
+    {
+        id: 9,
+        title: "Tellus duis neque fringilla pulvinar gravida",
+        price: "4.2",
+        avatar : ["09", "06", "08"],
+        time: "20:41",
+        likes: "10"
+    },
+    {
+        id: 10,
+        title: "Tristique diam a, enim, eros tellus. Viverra etiam",
+        price: "7.2",
+        avatar : ["09", "06", "08"],
+        time: "15:11",
+        likes: "70"
+    },
+    {
+        id: 11,
+        title: "Mattis at diam lorem nisl nam sed sociis",
+        price: "10.21",
+        avatar : ["09", "06", "08"],
+        time: "7:01",
+        likes: "16"
+    },
+    {
+        id: 12,
+        title: "Diam purus sagittis nisl erat mauris",
+        price: "41.22",
+        avatar : ["09", "06", "08"],
+        time: "3:21",
+        likes: "20"
+    },
+    {
+        id: 13,
+        title: "Senectus adipiscing nascetur accumsan etiam",
+        price: "4.2",
+        avatar : ["09", "06", "08"],
+        time: "2:41",
+        likes: "43"
+    },
+    {
+        id: 14,
+        title: "Dui accumsan leo vestibulum ornare eu",
+        price: "6.2",
+        avatar : ["09", "06", "08"],
+        time: "9:41",
+        likes: "19"
+    },
+    {
+        id: 15,
+        title: "Tristique diam a, enim, eros tellus. Viverra etiam",
+        price: "3.85",
+        avatar : ["09", "06", "08"],
+        time: "1:41",
+        likes: "30"
+    },
+    {
+        id: 16,
+        title: "Tellus duis neque fringilla pulvinar gravida",
+        price: "4.2",
+        avatar : ["09", "06", "08"],
+        time: "20:41",
+        likes: "10"
+    },
+    {
+        id: 17,
+        title: "Tristique diam a, enim, eros tellus. Viverra etiam",
+        price: "7.2",
+        avatar : ["09", "06", "08"],
+        time: "15:11",
+        likes: "70"
+    },
+    {
+        id: 18,
+        title: "Mattis at diam lorem nisl nam sed sociis",
+        price: "10.21",
+        avatar : ["09", "06", "08"],
+        time: "7:01",
+        likes: "16"
+    },
+    {
+        id: 19,
+        title: "Diam purus sagittis nisl erat mauris",
+        price: "41.22",
+        avatar : ["09", "06", "08"],
+        time: "3:21",
+        likes: "20"
     }
 ];
 
@@ -86,7 +215,7 @@ function loadAuction () {
                         <p><span class="h7 price-span" >${latestAuction.price} ETH</span></p>
                     </div>
                     <div class="time-div">
-                        <span><img src="images/icons/Timer Icon.svg" alt="Timer icon"></span>
+                        <span><i class='bx bx-stopwatch' ></i></span>
                         <span>${latestAuction.time} min left</span>
                     </div>
                     <div class="divider">
@@ -110,8 +239,8 @@ function loadAuction () {
 loadAuction ()
 
 //load hottest sale
-const hottestSaleDiv = document.getElementById("hottest-card-wrapper");
 function loadHottestSale() {
+    const hottestSaleDiv = document.getElementById("hottest-card-wrapper");
     latestAuctions.map((hottestSale)=> {
         const hottestSaleCard = document.createElement("div");
         hottestSaleCard.classList.add("hottest-card");
@@ -125,7 +254,7 @@ function loadHottestSale() {
                 </div>
                 <div class="time-price-div">
                     <div class="time-div">
-                        <span><img src="images/icons/Timer Icon.svg" alt="Timer icon"></span>
+                        <span><i class='bx bx-stopwatch' ></i></span>
                         <span>${hottestSale.time}</span>
                     </div>
                     <p><span class="h8 price-span">${hottestSale.price} ETH</span></p>
@@ -162,7 +291,7 @@ function loadpopularAuction() {
                 </div>
                 <div class="time-price-div">
                     <div class="time-div">
-                        <span><img src="images/icons/Timer Icon.svg" alt="Timer icon"></span>
+                        <span><i class='bx bx-stopwatch' ></i></span>
                         <span>${popularAuction.time}</span>
                     </div>
                     <p><span class="h8 price-span">${popularAuction.price} ETH</span></p>
@@ -195,12 +324,12 @@ function loadtopNft() {
                 <img src="images/big/img (${topNft.id}).png" alt="latest-img-${topNft.id}" />
             </div>
             <div class="top-nft-right-col">
-                <div class="hottest-card-text-div">
+                <div class="top-nft-card-text-div">
                     <h3 class="h7">${topNft.title}</h3>
                 </div>
                 <div class="time-price-div">
                     <div class="time-div">
-                        <span><img src="images/icons/Timer Icon.svg" alt="Timer icon"></span>
+                        <span><i class='bx bx-stopwatch' ></i></span>
                         <span>${topNft.time}</span>
                     </div>
                     <p><span class="h8 price-span">${topNft.price} ETH</span></p>
@@ -233,8 +362,8 @@ function scrollImage () {
 scrollImage();
 
 
-// hottest card carousel
 
+// hottest card carousel
 function getSlideArray(slideContainer) {
     const slideContainerChildren = document.querySelector(slideContainer).children;
     const slideArray = Array.from(slideContainerChildren);
@@ -243,35 +372,47 @@ function getSlideArray(slideContainer) {
 function getSlideWidth(slide) {
     slide = document.querySelector(slide);
     const slideWidth = slide.getBoundingClientRect().width;
-    return slideWidth;
+    return  slideWidth;
 }
 function getSlidePosition(slideWidth, index) {
-    return slideWidth * index + "px";
+    return (slideWidth + 20) * index + "px";
 }
 function getBtn(btn) {
     btn = document.querySelector(btn);
     return btn;
 }
-function loadCarousel (slideContainer, btn) {
+function loadCarousel (slideContainer, slide, nextBtn, previousBtn) {
     const slideArray = getSlideArray(slideContainer);
-    const slideWidth = 200;
-    const nxtBtn = getBtn(btn);
+    const slideWidth = getSlideWidth(slide);
+    const nxtBtn = getBtn(nextBtn);
+    const prevBtn = getBtn(previousBtn);
     let currentIndex = 0;
 
     nxtBtn.addEventListener('click', () => {
         currentIndex++;
         const slidePosition = getSlidePosition(slideWidth, currentIndex);
-
         if (currentIndex >= slideArray.length) {
             return;
         }
-
         slideArray[currentIndex].style.opacity = "1";
         slideArray.forEach((item) => {
+            item.style.transform = ` translateX(-${slidePosition}`;
+        });
+    });
+    prevBtn.addEventListener('click', () => {
+        currentIndex--;
+        const slidePosition = getSlidePosition(slideWidth, currentIndex);
+        if (currentIndex < 0 ) {
+            return;
+        }
+        slideArray[currentIndex -1].style.opacity = "1";
+        slideArray.forEach((item) => {
             item.style.transform = ` translateX(-${slidePosition})`;
+            
         });
     });
 }
 
-loadCarousel ('#hottest-card-wrapper', '.hottest-card-next-btn');
-// loadCarousel ('#popular-auction-card-wrapper', '.popular-card');
+loadCarousel ('#hottest-card-wrapper', '.hottest-card', '.hottest-card-next-btn', '.hottest-card-previous-btn');
+loadCarousel ('#popular-auction-card-wrapper', '.popular-card', '.popular-card-next-btn', '.popular-card-previous-btn');
+// console.log (document.querySelector('.popular-card').attributes)
